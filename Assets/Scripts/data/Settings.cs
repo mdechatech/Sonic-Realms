ce2cb1695c91e68853505ce6d281aa69e18163b5
mdechatech <mdechatech@gmail.com>
2015-01-01 20:02:36 -0600

    Added DefaultFixedDeltaTime

--- Assets/Scripts/data/Settings.cs
+++ Assets/Scripts/data/Settings.cs
@@ -8,4 +8,9 @@ public class Settings
 	public static KeyCode LeftKey = KeyCode.A;
 	public static KeyCode RightKey = KeyCode.D;
 	public static KeyCode JumpKey = KeyCode.W;
+
+    /// <summary>
+    /// The normal desired interval between fixed updates.
+    /// </summary>
+    public static float DefaultFixedDeltaTime = 0.02f;
 }
