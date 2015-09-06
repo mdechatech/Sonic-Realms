dff610441198fd3b39a945771662efe49caacadd
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-06 05:47:39 -0500

    Add dedicated LinecastNonAlloc method

--- Hedgehog/Scripts/Utils/Physics2DUtility.cs
+++ Hedgehog/Scripts/Utils/Physics2DUtility.cs
@@ -6,12 +6,39 @@ using UnityEngine;
 
 namespace Hedgehog.Utils
 {
-    public class Physics2DUtility
+    public static class Physics2DUtility
     {
+        #region Raycast Allocation Variables
+        private const int MaxRaycastResults = 16;
+        private static readonly RaycastHit2D[] RaycastResults = new RaycastHit2D[MaxRaycastResults];
+
+        private static int _previousRaycastResultAmount;
+        private static int _raycastResultAmount = 0;
+        #endregion
+
+        public static RaycastHit2D[] LinecastNonAlloc(Vector2 start, Vector2 end,
+            int layerMask = Physics2D.DefaultRaycastLayers)
+        {
+            _previousRaycastResultAmount = _raycastResultAmount;
+            _raycastResultAmount = Physics2D.LinecastNonAlloc(start, end, RaycastResults, layerMask);
+
+            if (_raycastResultAmount < _previousRaycastResultAmount)
+            {
+                for (var i = _raycastResultAmount;
+                    i < _previousRaycastResultAmount;
+                    i++)
+                {
+                    RaycastResults[i] = default(RaycastHit2D);
+                }
+            }
+
+            return RaycastResults;
+        }
+
         public static RaycastHit2D ClosestWithTag(IEnumerable<RaycastHit2D> raycastHit2Ds, ICollection<string> tags)
         {
             if (!raycastHit2Ds.Any()) return default(RaycastHit2D);
-            return raycastHit2Ds.FirstOrDefault(raycastHit2D => tags.Contains(raycastHit2D.collider.tag));
+            return raycastHit2Ds.FirstOrDefault(raycastHit2D => raycastHit2D && tags.Contains(raycastHit2D.collider.tag));
         }
 
         public static RaycastHit2D ClosestWithTag(IEnumerable<RaycastHit2D> raycastHit2Ds, string tag)
