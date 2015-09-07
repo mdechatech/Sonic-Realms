6d031908d528374a0683d9585ecce2da883733a4
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-07 04:24:57 -0500

    Brand new example scenes and scripts

---
+++ Hedgehog/Examples/Scripts/ExamplePlatformMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Hedgehog.Examples
+{
+    public class ExamplePlatformMover : MonoBehaviour
+    {
+        public float dx;
+        public float dy;
+
+        public void FixedUpdate()
+        {
+            transform.position += new Vector3(dx, dy) * Time.fixedDeltaTime;
+        }
+    }
+}
