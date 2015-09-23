10043098e5226ac9ec6cbcebf8c33cb4da01702d
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-23 10:16:29 -0500

    Implement ledges in new trigger system

---
+++ Hedgehog/Scripts/Terrain/Ledge.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Hedgehog.Terrain
+{
+    [RequireComponent(typeof(PlatformTrigger))]
+    public class Ledge : MonoBehaviour
+    {
+        public void OnEnable()
+        {
+            GetComponent<PlatformTrigger>().CollisionPredicates.Add(Selector);
+        }
+
+        public void OnDisable()
+        {
+            GetComponent<PlatformTrigger>().CollisionPredicates.Remove(Selector);
+        }
+
+        public static bool Selector(TerrainCastHit hit)
+        {
+            if(hit.Source == null) 
+                return (hit.Side & TerrainSide.Bottom) > 0;
+
+            // Check must be coming from player's bottom side and be close to the top
+            // of the platform
+            return (hit.Side & TerrainSide.Bottom) > 0 && hit.Hit.fraction > 0.0f;
+        }
+    }
+}
