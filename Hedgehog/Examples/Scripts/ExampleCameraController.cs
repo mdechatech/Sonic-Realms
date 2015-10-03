824d481dc424f5714bbff5f34dd4f825d2a10700
Matthewit Dechatech <mdechatech@gmail.com>
2015-10-02 23:28:17 -0500

    Better camera controller

--- Hedgehog/Examples/Scripts/ExampleCameraController.cs
+++ Hedgehog/Examples/Scripts/ExampleCameraController.cs
@@ -1,43 +1,87 @@
+using Hedgehog.Core.Actors;
+using Hedgehog.Core.Utils;
 using UnityEngine;
 
 namespace Hedgehog.Examples
 {
+    /// <summary>
+    /// Example camera controller, good for catching up with fast hedgehogs.
+    /// </summary>
     public class ExampleCameraController : MonoBehaviour
     {
         /// <summary>
         /// The target for the camera to follow.
         /// </summary>
         [SerializeField]
-        public GameObject FollowTarget;
+        public Transform FollowTarget;
 
         /// <summary>
-        /// Must be 1 or higher. The higher this is, the smoother the camera movement.
+        /// How smoothly the target's position is followed, 1 being smoothest.
         /// </summary>
-        [SerializeField]
-        public float EaseFactor = 1;
+        [SerializeField, Range(0.0f, 1.0f)]
+        [Tooltip("How smoothly the target's position is followed, 1 being smoothest.")]
+        public float Smoothness;
 
         /// <summary>
-        /// Whether or not the camera stays put in terms of depth.
+        /// Whether to rotate toward the follow target's direction of gravity, if it is a controller.
         /// </summary>
         [SerializeField]
-        public bool ZLock;
+        [Tooltip("Whether to rotate toward the follow target's direction of gravity, if it is a controller.")]
+        public bool RotateToGravity;
 
         /// <summary>
-        /// If the z lock is off, the amount in distance to maintain from the target in terms of depth.
+        /// How smoothly the target is followed, 1 being smoothest.
         /// </summary>
-        [SerializeField]
-        public float ZOffset;
+        [SerializeField, Range(0.0f, 1.0f)]
+        [Tooltip("How smoothly the target's rotation is followed, 1 being smoothest.")]
+        public float RotationSmoothness;
+
+        public void Reset()
+        {
+            Smoothness = 0.0f;
+            RotationSmoothness = 0.2f;
+            RotateToGravity = true;
+        }
 
         public void FixedUpdate()
         {
-            if(FollowTarget != null)
+            if (FollowTarget == null) return;
+            var hedgehog = FollowTarget.GetComponent<HedgehogController>();
+
+            if (DMath.Equalsf(Smoothness))
             {
-                Camera.main.transform.position = new Vector3 (
-                    Camera.main.transform.position.x + (FollowTarget.transform.position.x - Camera.main.transform.position.x) / EaseFactor, 
-                    Camera.main.transform.position.y + (FollowTarget.transform.position.y - Camera.main.transform.position.y) / EaseFactor,
-                    (ZLock) ? 
-                        Camera.main.transform.position.z : 
-                        Camera.main.transform.position.z + (FollowTarget.transform.position.z - ZOffset) / EaseFactor);
+                Camera.main.transform.position = new Vector3(FollowTarget.transform.position.x,
+                    FollowTarget.transform.position.y,
+                    Camera.main.transform.position.z);
+            }
+            else
+            {
+                Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position,
+                    new Vector3(FollowTarget.transform.position.x, FollowTarget.transform.position.y,
+                        Camera.main.transform.position.z),
+                    Time.fixedDeltaTime * (1.0f / Smoothness)
+                    );
+            }
+
+            if (RotateToGravity && hedgehog != null)
+            {
+                if (DMath.Equalsf(RotationSmoothness))
+                {
+                    Camera.main.transform.eulerAngles = new Vector3(
+                        Camera.main.transform.eulerAngles.x,
+                        Camera.main.transform.eulerAngles.y,
+                        RotateToGravity ? hedgehog.GravityDirection + 90.0f : 0.0f);
+                }
+                else
+                {
+                    Camera.main.transform.eulerAngles = new Vector3(
+                        Camera.main.transform.eulerAngles.x,
+                        Camera.main.transform.eulerAngles.y,
+                        Mathf.LerpAngle(
+                            Camera.main.transform.eulerAngles.z,
+                            RotateToGravity ? hedgehog.GravityDirection + 90.0f : 0.0f,
+                            Time.fixedDeltaTime*(1.0f/RotationSmoothness)));
+                }
             }
         }
     }
