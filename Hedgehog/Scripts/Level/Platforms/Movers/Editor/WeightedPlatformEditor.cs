e84b94078516e126fdf6e3f69cc4f79b0f022a08
Matthewit Dechatech <mdechatech@gmail.com>
2015-10-12 18:46:03 -0500

    Add a proper collision tileset

---
+++ Hedgehog/Scripts/Level/Platforms/Movers/Editor/WeightedPlatformEditor.cs
@@ -0,0 +1,20 @@
+﻿using Hedgehog.Core.Utils.Editor;
+using UnityEditor;
+
+namespace Hedgehog.Level.Platforms.Movers.Editor
+{
+    [CustomEditor(typeof(WeightedPlatform))]
+    public class WeightedPlatformEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            HedgehogEditorGUIUtility.DrawExcluding(serializedObject,
+                "ReverseDirection",
+                "PingPong");
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
