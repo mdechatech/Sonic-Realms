b604bf53cf044becb073f7cf03340ed76e9305eb
Matthewit Dechatech <mdechatech@gmail.com>
2016-10-11 05:36:11 -0500

    Add palette scriptable object, palette partitions, and custom editor

---
+++ Assets/Scripts/SonicRealms/Core/Utils/Editor/PaletteLineEditor.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace SonicRealms.Core.Utils.Editor
+{
+    /// <summary>
+    /// BUG The editor ballses up when Reset is called
+    /// </summary>
+    [CustomEditor(typeof(PaletteLine))]
+    public class PaletteLineEditor : BaseRealmsEditor
+    {
+        private PaletteLine _instance;
+
+        private ReorderableList _mainLineRList;
+        private List<ReorderableList> _partitionRLists;
+
+        private bool[] _indexButtonStates;
+
+        protected bool ShowCreatePartitionFoldout
+        {
+            get { return EditorPrefs.GetBool("PaletteLineEditor.ShowCreatePartitionFoldout", false); }
+            set { EditorPrefs.SetBool("PaletteLineEditor.ShowCreatePartitionFoldout", value); }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _instance = (PaletteLine) target;
+
+            InitializeMainLineRList();
+            InitializePartitionRLists();
+
+            _indexButtonStates = new bool[_instance.MaxColors];
+
+            serializedObject.Update();
+        }
+
+        protected override void DrawProperty(PropertyData property, GUIContent label)
+        {
+            if (property.Name == "_maxColors")
+            {
+                base.DrawProperty(property, label);
+
+                if (GUILayout.Button("Resize Palette"))
+                {
+                    if (EditorUtility.DisplayDialog("Resize Palette?",
+                        "Shrinking the palette size will result in loss of " +
+                        "data. Are you sure you want to resize the palette?",
+                        "Yes", "No"))
+                    {
+                        Undo.RecordObject(_instance, "Resize Palette");
+                        _instance.ResizePalette(_instance.MaxColors);
+                        OnEnable();
+                    }
+                }
+                else if (_instance.IsDirty)
+                {
+                    EditorGUILayout.HelpBox("Max Colors has changed. The palette must be resized before any " +
+                                            "further changes can be made.", MessageType.Info);
+                }
+            }
+            else if (_instance.IsDirty)
+            {
+                return;
+            }
+            else if (property.Name == "_mainLine")
+            {
+                if (_instance.MainLine.EntryCount != _mainLineRList.count)
+                    OnEnable();
+
+                _mainLineRList.DoLayoutList();
+            }
+            else if (property.Name == "_partitions")
+            {
+                if (_partitionRLists.Count != _instance.PartitionCount)
+                    OnEnable();
+
+                for(var i = 0; i < _partitionRLists.Count; ++i)
+                {
+                    if (GUILayout.Button("Delete Partition " + i))
+                    {
+                        Undo.RecordObject(_instance, "Delete Partition");
+
+                        _instance.RemovePartitionAt(i);
+                        serializedObject.Update();
+                        OnEnable();
+
+                        return;
+                    }
+
+                    _partitionRLists[i].DoLayoutList();
+                }
+            }
+            else
+            {
+                base.DrawProperty(property, label);
+            }
+        }
+
+        protected override bool DrawFoldout(bool state, FoldoutData foldout)
+        {
+            if (_instance.IsDirty)
+                return state;
+
+            if (foldout.Name == "Partitions")
+            {
+                if (ShowCreatePartitionFoldout = EditorGUILayout.Foldout(ShowCreatePartitionFoldout, "Create Partition"))
+                {
+                    DrawCreatePartition();
+                }
+
+                return base.DrawFoldout(state, foldout);
+            }
+            else
+            {
+                return base.DrawFoldout(state, foldout);
+            }
+        }
+
+        private void DrawCreatePartition()
+        {
+            EditorGUILayout.LabelField("Select Indices");
+
+            GUILayout.BeginHorizontal();
+
+            if (_indexButtonStates.Length != _instance.MaxColors)
+                Array.Resize(ref _indexButtonStates, _instance.MaxColors);
+
+            for (var i = 0; i < _instance.MaxColors; ++i)
+            {
+                var partitionIndex = _instance.GetIndexOfPartitionFor(i);
+                if (partitionIndex >= 0)
+                {
+                    GUI.enabled = false;
+
+                    _indexButtonStates[i] = false;
+
+                    GUILayout.Button(string.Format("{0}({1})", i, partitionIndex),
+                        new GUIStyle(EditorStyles.toolbarButton));
+
+                    GUI.enabled = true;
+                }
+                else
+                {
+                    var indexState = _indexButtonStates[i];
+                    if (!indexState)
+                    {
+                        if (GUILayout.Button(i.ToString(), new GUIStyle(EditorStyles.toolbarButton)))
+                        {
+                            _indexButtonStates[i] = true;
+                        }
+                    }
+                    else
+                    {
+                        if (GUILayout.Button(i.ToString(),
+                            new GUIStyle(EditorStyles.toolbarButton) { normal = EditorStyles.toolbarButton.active }))
+                        {
+                            _indexButtonStates[i] = false;
+                        }
+                    }
+                }
+            }
+
+            GUILayout.EndHorizontal();
+            
+            var indices = new HashSet<int>();
+
+            for (var i = 0; i < _indexButtonStates.Length; ++i)
+            {
+                if (_indexButtonStates[i])
+                    indices.Add(i);
+            }
+
+            GUI.enabled = indices.Count > 0;
+
+            if (GUILayout.Button("Create"))
+            {
+                Undo.RecordObject(_instance, "Create Palette Partition");
+
+                _instance.AddBlankPartition(indices);
+                serializedObject.Update();
+
+                InitializePartitionRLists();
+            }
+
+            GUI.enabled = true;
+        }
+
+        private void InitializeMainLineRList()
+        {
+            var mainLineProp = serializedObject.FindProperty("_mainLine");
+
+            _mainLineRList = CreatePartitionRList(mainLineProp, _instance.MainLine, "Main Line");
+        }
+
+        private void InitializePartitionRLists()
+        {
+            _partitionRLists = new List<ReorderableList>();
+
+            var partitionsProp = serializedObject.FindProperty("_partitions");
+
+            for (var i = 0; i < _instance.PartitionCount; ++i)
+            {
+                var partitionProp = partitionsProp.GetArrayElementAtIndex(i);
+
+                _partitionRLists.Add(CreatePartitionRList(partitionProp, _instance.GetPartitionAt(i),
+                    "Partition " + i));
+            }
+        }
+
+        private ReorderableList CreatePartitionRList(SerializedProperty partitionProp, PaletteLine.Partition partition,
+            string name)
+        {
+            var entriesProp = partitionProp.FindPropertyRelative("_entries");
+
+            var rlist = new ReorderableList(
+                serializedObject,
+                entriesProp);
+
+            // Color dimensions
+            const int colorLabelWidth = 20;
+            const int colorWidth = 34;
+            const int colorHeight = 17;
+            const int colorsPerRow = 8;
+
+            rlist.elementHeight = Mathf.Max(colorHeight * 2,
+                colorHeight * Mathf.CeilToInt(_instance.MaxColors / (float)colorsPerRow));
+
+            // Header
+            rlist.drawHeaderCallback = rect =>
+            {
+                EditorGUI.LabelField(rect, name);
+            };
+
+            // Add
+            rlist.onAddCallback = list =>
+            {
+                Undo.RecordObject(_instance, "Add Palette Line Entry");
+                partition.CopyAppendLastEntries();
+            };
+
+            // Remove
+            rlist.onRemoveCallback = list =>
+            {
+                Undo.RecordObject(_instance, string.Format("Remove Partition Entry"));
+                partition.RemoveEntriesAt(list.index);
+            };
+
+            // Draw
+            rlist.drawElementCallback = (rect, index, active, focused) =>
+            {
+                var entries = partition.GetEntriesAt(index);
+
+                var curX = rect.x;
+                var curY = rect.y;
+
+                // Label
+                const int labelWidth = 17;
+
+                EditorGUI.LabelField(new Rect(curX, curY, labelWidth, rect.height),
+                    index.ToString(),
+                    new GUIStyle(EditorStyles.boldLabel) { alignment = TextAnchor.MiddleLeft });
+
+                curX += labelWidth;
+
+                // Buttons
+                const int ButtonsWidth = 34;
+                const int ButtonsPad = 5;
+
+                if (GUI.Button(new Rect(curX, curY, ButtonsWidth, (rect.height - ButtonsPad) * 0.5f),
+                    "->"))
+                {
+                    Undo.RecordObject(_instance, "Cycle Colors Forward");
+                    entries.CycleForward();
+                }
+                else if (
+                    GUI.Button(
+                        new Rect(curX, curY + (rect.height - ButtonsPad) * 0.5f, ButtonsWidth,
+                            (rect.height - ButtonsPad) * 0.5f),
+                        "<-"))
+                {
+                    Undo.RecordObject(_instance, "Cycle Colors Backward");
+                    entries.CycleBackward();
+                }
+
+                curX += ButtonsWidth + 5;
+
+                var i = 0;
+                foreach (var colorIndex in partition.GetIndices())
+                {
+                    if (colorIndex >= _instance.MaxColors)
+                        continue;
+
+                    var entry = entries[i];
+                    
+                    // Color label
+                    EditorGUI.LabelField(
+                        new Rect(curX, curY, colorLabelWidth, colorHeight),
+                        colorIndex.ToString());
+
+                    curX += colorLabelWidth;
+
+                    // Color field
+                    var color = EditorGUI.ColorField(new Rect(curX, curY, colorWidth, colorHeight),
+                        entry);
+
+                    if (color != entries[i])
+                    {
+                        Undo.RecordObject(_instance, "Set Partition Entry Color");
+                        entries[i] = color;
+                    }
+
+
+                    curX += colorWidth;
+
+                    if ((i + 1) % colorsPerRow == 0)
+                    {
+                        curX -= (colorLabelWidth + colorWidth) * colorsPerRow;
+                        curY += colorHeight;
+                    }
+
+                    ++i;
+                }
+            };
+
+            return rlist;
+        }
+    }
+}
---
+++ Assets/Scripts/SonicRealms/Core/Utils/PaletteLine.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SonicRealms.Core.Utils
+{
+    /// <summary>
+    /// <para>Holds palette data for <see cref="PaletteCycler"/>.</para>
+    /// <para>It has a primary palette line <see cref="MainLine"/> and can also partition parts of it for
+    /// a greater degree of control. For more info on partitions, see <see cref="GetPartitionAt"/>.</para>
+    /// </summary>
+    [CreateAssetMenu(fileName = "Palette Line", menuName = "Sonic Realms/Palette Line")]
+    public class PaletteLine : ScriptableObject
+    {
+        #region Public Fields & Properties
+
+        public const int DefaultMaxColors = 16;
+
+        /// <summary>
+        /// For internal editor use.
+        /// </summary>
+        public bool IsDirty { get { return _prevMaxColors != _maxColors; } }
+
+        /// <summary>
+        /// Number of colors each entry in the palette line holds.
+        /// </summary>
+        public int MaxColors { get { return _maxColors; } }
+
+        /// <summary>
+        /// The main palette line. Each entry holds as many colors as specified by <see cref="MaxColors"/>.
+        /// </summary>
+        public Partition MainLine { get { return _mainLine; } set { _mainLine = value; } }
+
+        /// <summary>
+        /// Number of partitions on the main palette line.
+        /// </summary>
+        public int PartitionCount { get { return _partitions.Count; } }
+
+        #endregion
+
+        #region Private & Inspector Fields
+
+        [SerializeField, HideInInspector]
+        private int _prevMaxColors;
+
+        [SerializeField]
+        [Range(1, DefaultMaxColors)]
+        private int _maxColors;
+
+        [SerializeField]
+        [Foldout("Main Line")]
+        private Partition _mainLine;
+
+        [SerializeField]
+        [Foldout("Partitions")]
+        private List<Partition> _partitions;
+
+        #endregion
+
+        #region Public & Helper Functions
+
+        /// <summary>
+        /// Clears the main palette line and all partitioned palette lines.
+        /// </summary>
+        public void ClearPalette()
+        {
+            _mainLine = new Partition(_maxColors);
+            _partitions.Clear();
+        }
+
+        /// <summary>
+        /// Resizes the main palette line to hold the given number of colors. WATCH OUT! Passing in a
+        /// lower number from the current <see cref="MaxColors"/> results in the extra colors getting
+        /// trashed.
+        /// </summary>
+        public void ResizePalette(int count)
+        {
+            _prevMaxColors = _maxColors = count;
+
+            MainLine.ResizeEntries(count);
+        }
+
+        /// <summary>
+        /// <para>
+        /// Returns the partition at the given index. Use <see cref="PartitionCount"/> for bounds-checking.
+        /// </para>
+        /// <para>
+        /// A partition is an extra palette line that takes up parts of the <see cref="MainLine"/>. It can
+        /// be cycled independently of the main palette line and has its own separate entries.
+        /// </para>
+        /// </summary>
+        public Partition GetPartitionAt(int index)
+        {
+            return _partitions[index];
+        }
+
+        /// <summary>
+        /// Returns the partition that is taking up the given color index. For more info on partitions, see
+        /// <see cref="GetPartitionAt"/>.
+        /// <returns>The index of the partition, or -1 if the <see cref="MainLine"/> is using the color index.</returns>
+        /// </summary>
+        public int GetIndexOfPartitionFor(int colorIndex)
+        {
+            for (var i = 0; i < _partitions.Count; ++i)
+            {
+                var partition = _partitions[i];
+
+                if (partition.HasIndex(colorIndex))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the partition that is taking up the given color index. For more info on partitions, see
+        /// <see cref="GetPartitionAt"/>.
+        /// <returns>The partition, <see cref="MainLine"/> is using the color index.</returns>
+        /// </summary>
+        public Partition GetPartitionFor(int colorIndex)
+        {
+            for (var i = 0; i < _partitions.Count; ++i)
+            {
+                var partition = _partitions[i];
+
+                if (partition.HasIndex(colorIndex))
+                    return partition;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Creates a blank partition from the <see cref="MainLine"/> that takes up the given color indices.
+        /// For more info on partitions, see <see cref="GetPartitionAt"/>.
+        /// </summary>
+        public void AddBlankPartition(HashSet<int> colorIndices)
+        {
+            _partitions.Add(new Partition(colorIndices));
+        }
+
+        /// <summary>
+        /// Inserts a blank partition from the <see cref="MainLine"/> that takes up the given color indices.
+        /// For more info on partitions, see <see cref="GetPartitionAt"/>.
+        /// </summary>
+        public void InsertBlankPartition(int index, HashSet<int> colorIndices)
+        {
+            _partitions.Insert(index, new Partition(colorIndices));
+        }
+
+        /// <summary>
+        /// Frees up the partition at the given index. Use <see cref="PartitionCount"/> for bounds checking.
+        /// For more info on partitions, see <see cref="GetPartitionAt"/>.
+        /// </summary>
+        public void RemovePartitionAt(int index)
+        {
+            _partitions.RemoveAt(index);
+        }
+
+        #endregion
+
+        #region Lifecycle Functions
+
+        protected void Reset()
+        {
+            _prevMaxColors = _maxColors = 16;
+            _mainLine = new Partition(_maxColors);
+        }
+
+        #endregion
+
+        #region Nested Classes
+
+        [Serializable]
+        public class Partition
+        {
+            [SerializeField]
+            private List<int> _indices;
+
+            [SerializeField]
+            private List<ColorList> _entries;
+
+            [SerializeField]
+            private int _listLength;
+
+            public int ListLength { get { return _listLength; } }
+
+            public int EntryCount { get { return _entries.Count; } }
+
+            public Partition(int listLength) : this(new HashSet<int>(Enumerable.Range(0, listLength)))
+            {
+
+            }
+
+            public Partition(HashSet<int> indices)
+            {
+                _listLength = indices.Count;
+                _indices = indices.ToList();
+                _entries = new List<ColorList>();
+            }
+
+            public bool HasIndex(int index)
+            {
+                return _indices.Contains(index);
+            }
+
+            public HashSet<int> GetIndices()
+            {
+                return new HashSet<int>(_indices);
+            }
+
+            public void AddBlankEntries()
+            {
+                _entries.Add(new ColorList(_listLength));
+            }
+
+            public void CopyAppendLastEntries()
+            {
+                var list = new ColorList(_listLength);
+
+                if (_entries.Count > 0)
+                {
+                    var lastList = _entries[_entries.Count - 1];
+
+                    for (var i = 0; i < _listLength; ++i)
+                    {
+                        list[i] = lastList[i];
+                    }
+                }
+
+                _entries.Add(list);
+            }
+
+            public void InsertBlankEntries(int index)
+            {
+                _entries.Insert(index, new ColorList(_listLength));
+            }
+
+            public ColorList GetEntriesAt(int index)
+            {
+                return _entries[index];
+            }
+
+            public void RemoveEntriesAt(int index)
+            {
+                _entries.RemoveAt(index);
+            }
+
+            public void ClearEntries()
+            {
+                _entries.Clear();
+            }
+
+            public void ResizeEntries(int count)
+            {
+                _listLength = count;
+
+                foreach (var entry in _entries)
+                {
+                    entry.Resize(count);
+                }
+            }
+
+            [Serializable]
+            public class ColorList
+            {
+                public int Count { get { return _list.Length; } }
+
+                public Color this[int index] { get { return _list[index]; } set { _list[index] = value; } }
+
+                [SerializeField]
+                private Color[] _list;
+
+                public ColorList(int count)
+                {
+                    _list = new Color[count];
+                }
+
+                public void Clear()
+                {
+                    for (var i = 0; i < _list.Length; ++i)
+                        _list[i] = new Color();
+                }
+
+                public void Resize(int count)
+                {
+                    Array.Resize(ref _list, count);
+                }
+
+                public void CycleForward()
+                {
+                    var last = _list[_list.Length - 1];
+
+                    for (var i = _list.Length - 2; i >= 0; --i)
+                    {
+                        _list[i + 1] = _list[i];
+                    }
+
+                    _list[0] = last;
+                }
+
+                public void CycleBackward()
+                {
+                    var first = _list[0];
+
+                    for (var i = 1; i < _list.Length; ++i)
+                    {
+                        _list[i - 1] = _list[i];
+                    }
+
+                    _list[_list.Length - 1] = first;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
